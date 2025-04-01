1ada9284cb15f8ce4f483244e033068418454413
Adrián González Blanco <z3u2Thnder@proton.me>
2025-04-01 03:16:36 +0200

    Reset values in data structure to zero for all nodes in the tree

--- data.js
+++ data.js
@@ -5,51 +5,51 @@ const data = {
     children: [
       {
         name: "Devengos",
-        value: 0.5,
+        value: 0.0,
         info: "Información sobre devengos",
         canBeDeleted: false,
         isContingenciaProfesional: false,
         children: [
           {
             name: "Percepciones Salariales",
-            value: 1.2,
+            value: 0.0,
             info: "Detalles sobre percepciones",
             canBeDeleted: false,
             isContingenciaProfesional: false,
             children: [
-              { name: "Salario Base", value: 1.23, info: "Salario base mensual", canBeDeleted: false, isContingenciaProfesional: false },
-              { name: "Complementos Salariales", value: 0.5, info: "Extras salariales", canBeDeleted: true, isContingenciaProfesional: false }
+              { name: "Salario Base", value: 0.0, info: "Salario base mensual", canBeDeleted: false, isContingenciaProfesional: false },
+              { name: "Complementos Salariales", value: 0.0, info: "Extras salariales", canBeDeleted: true, isContingenciaProfesional: false }
             ]
           },
           {
             name: "Otras Percepciones",
-            value: 0.8,
+            value: 0.0,
             info: "Información adicional",
             canBeDeleted: true,
             isContingenciaProfesional: false,
             children: [
-              { name: "Suplidos", value: 2.45, info: "Gastos reembolsados", canBeDeleted: true, isContingenciaProfesional: false },
-              { name: "Prestaciones SS", value: -0.75, info: "Deducción por SS", canBeDeleted: false, isContingenciaProfesional: true },
-              { name: "Otras", value: -0.75, info: "Otras deducciones", canBeDeleted: true, isContingenciaProfesional: false }
+              { name: "Suplidos", value: 0.0, info: "Gastos reembolsados", canBeDeleted: true, isContingenciaProfesional: false },
+              { name: "Prestaciones SS", value: 0.0, info: "Deducción por SS", canBeDeleted: false, isContingenciaProfesional: true },
+              { name: "Otras", value: 0.0, info: "Otras deducciones", canBeDeleted: true, isContingenciaProfesional: false }
             ]
           }
         ]
       },
       {
         name: "Deducciones",
-        value: -0.2,
+        value: 0.0,
         info: "Deducciones aplicadas",
         children: [
           {
             name: "Seguridad Social",
-            value: 0.9,
+            value: 0.0,
             info: "Pago a la seguridad social",
             children: [
-              { name: "Contingencias Comunes", value: 1.75, info: "Deducción por contingencias" },
-              { name: "MEI", value: -0.5, info: "Deducción MEI" }
+              { name: "Contingencias Comunes", value: 0.0, info: "Deducción por contingencias" },
+              { name: "MEI", value: 0.0, info: "Deducción MEI" }
             ]
           },
-          { name: "IRPF", value: -0.8, info: "Impuesto sobre la renta" },
+          { name: "IRPF", value: 0.0, info: "Impuesto sobre la renta" },
           { name: "Otros", value: 0.0, info: "Otros descuentos" }
         ]
       }
