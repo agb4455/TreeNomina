8f2ff22283c1250e23c23786755bfc13b96b3371
Adrián González Blanco <z3u2Thnder@proton.me>
2025-04-06 23:43:47 +0200

    Implement PDF filling functions for salary perceptions and update HTML structure

--- script.js
+++ script.js
@@ -389,5 +389,60 @@ document.getElementById("closeModalBtn").addEventListener("click", () => {
   document.getElementById("myModal").style.display = "none";
 });
 
+function fillPdf(){
+  fillSalaryPerceptions();
+}
+
+function fillSalaryPerceptions(){
+  fillBase();
+  fillBonus();
+  fillOther();
+}
+
+function fillBase(){
+  const node = findNodeByName(data,"Salario Base")
+  console.log(node);
+  const container = document.getElementById("percepciones-salariales-base");
+  container.innerHTML = ""; // Clear previous content
+  const nValue = parseFloat(node.value) + " €";
+  const fila = document.createElement("tr");
+  fila.innerHTML = `<td class = "td-pdf" colspan="5">${node.name}</td><td class = "td-pdf" >${nValue}</td>`;
+  container.appendChild(fila);
+}
+
+function fillBonus(){
+  const node = findNodeByName(data,"Complementos Salariales")
+  console.log(node);
+  const container = document.getElementById("percepciones-salariales-complementos");
+  container.innerHTML = ""; // Clear previous content
+  node.children.forEach(child => {
+      const nValue = parseFloat(child.value) + " €";
+      const fila = document.createElement("tr");
+      fila.innerHTML = `<td class = "td-pdf" colspan="5">${child.name}</td><td class = "td-pdf">${nValue}</td>`;
+      container.appendChild(fila);
+  });
+}
+
+function fillOther(){
+  const node = findNodeInRootByName(root,"Otras Percepciones");
+  console.log(node);
+  const container = document.getElementById("percepciones-no-salariales");
+  container.innerHTML = ""; // Clear previous content
+  node.children.forEach(percepcion => {
+      const fila = document.createElement("tr");
+      fila.innerHTML = `<td class = "td-pdf" colspan="6">${percepcion.data.name}</td>`;
+      container.appendChild(fila);
+      if(percepcion.data.children.length !== 0){
+          percepcion.children.forEach(child => {
+              console.log(child);
+              const nValue = parseFloat(child.value) + " €";
+              const fila = document.createElement("tr");
+              fila.innerHTML = `<td class = "td-pdf" colspan="5">${child.name}</td><td class = "td-pdf">${nValue}</td>`;
+              container.appendChild(fila);
+          });
+      }
+  });
+}
+
 // Renderizar el árbol inicialmente
 renderTree();
